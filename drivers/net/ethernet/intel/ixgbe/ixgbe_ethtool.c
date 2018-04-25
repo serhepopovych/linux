5f454d366a6929642b7b304a9d197b21b3ecb218
Serhey Popovych <serhe.popovych@gmail.com>
2018-04-25 21:40:58 +0300

    ixgbe: Add DMATXCTL and EXVET registers to ethtool dump
    To ease code debugging and troubleshooting add DMATXCTL and EXVET
    registers to ethtool(8) register dump. One might use following command
    to list contents of registers:
    
      # ethtool --register-dump eth2
    
    Following registers might be useful for Double VLAN mode:
    
      0x05088: VLNCTRL     (VLAN Control register)
      0x00018: CTRL_EXT    (Extended Device Control)
      0x04A80: DMATXCTL    (DMA Tx Control)
      0x05078: EXVET       (Extended VLAN Ether Type)
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_ethtool.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_ethtool.c
@@ -515,7 +515,7 @@ static void ixgbe_set_msglevel(struct net_device *netdev, u32 data)
 
 static int ixgbe_get_regs_len(struct net_device *netdev)
 {
-#define IXGBE_REGS_LEN  1145
+#define IXGBE_REGS_LEN  1147
 	return IXGBE_REGS_LEN * sizeof(u32);
 }
 
@@ -886,6 +886,20 @@ static void ixgbe_get_regs(struct net_device *netdev,
 	regs_buff[1142] = IXGBE_READ_REG(hw, IXGBE_SECTXMINIFG);
 	regs_buff[1143] = IXGBE_READ_REG(hw, IXGBE_SECRXCTRL);
 	regs_buff[1144] = IXGBE_READ_REG(hw, IXGBE_SECRXSTAT);
+
+	/* 82599+ specific GDV registers */
+	switch (hw->mac.type) {
+	case ixgbe_mac_82599EB:
+	case ixgbe_mac_X540:
+	case ixgbe_mac_X550:
+	case ixgbe_mac_X550EM_x:
+	case ixgbe_mac_x550em_a:
+		regs_buff[1145] = IXGBE_READ_REG(hw, IXGBE_DMATXCTL);
+		regs_buff[1146] = IXGBE_READ_REG(hw, IXGBE_EXVET);
+		break;
+	default:
+		break;
+	}
 }
 
 static int ixgbe_get_eeprom_len(struct net_device *netdev)
