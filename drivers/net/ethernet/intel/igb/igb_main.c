serhepopovych/igb

igb: Introduce and use igb_do_reset()
There are two places sharing common code snippet and possibly will be
more in future: move it to new function igb_do_reset() to avoid code
duplication.

This function is similar to one found in ixgbe driver.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

src/igb_main.c
@@ -2096,6 +2096,16 @@ void igb_reset(struct igb_adapter *adapter)
 	adapter->devrc++;
 }
 
+void igb_do_reset(struct net_device *netdev)
+{
+	struct igb_adapter *adapter = netdev_priv(netdev);
+
+	if (netif_running(netdev))
+		igb_reinit_locked(adapter);
+	else
+		igb_reset(adapter);
+}
+
 #ifdef HAVE_NDO_SET_FEATURES
 #ifdef HAVE_RHEL6_NET_DEVICE_OPS_EXT
 static u32 igb_fix_features(struct net_device *netdev,
@@ -2153,10 +2163,7 @@ static int igb_set_features(struct net_device *netdev,
 
 	netdev->features = features;
 
-	if (netif_running(netdev))
-		igb_reinit_locked(adapter);
-	else
-		igb_reset(adapter);
+	igb_do_reset(netdev);
 
 	return 0;
 }

serhepopovych/linux

drivers/net/ethernet/intel/igb/igb_main.c
@@ -2432,6 +2432,16 @@ void igb_reset(struct igb_adapter *adapter)
 	igb_get_phy_info(hw);
 }
 
+void igb_do_reset(struct net_device *netdev)
+{
+	struct igb_adapter *adapter = netdev_priv(netdev);
+
+	if (netif_running(netdev))
+		igb_reinit_locked(adapter);
+	else
+		igb_reset(adapter);
+}
+
 static netdev_features_t igb_fix_features(struct net_device *netdev,
 	netdev_features_t features)
 {
@@ -2475,10 +2485,7 @@ static int igb_set_features(struct net_device *netdev,
 
 	netdev->features = features;
 
-	if (netif_running(netdev))
-		igb_reinit_locked(adapter);
-	else
-		igb_reset(adapter);
+	igb_do_reset(netev);
 
 	return 1;
 }